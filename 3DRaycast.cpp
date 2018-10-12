88f7b075bd6335193f9cd72a0c31702b04253798
name-here <felixjyancey@mac.com>
2018-10-12 15:27:43 -0400

    Added camera rotation

--- 3DRaycast.cpp
+++ 3DRaycast.cpp
@@ -7,16 +7,17 @@ TODO:                                                                           
 	-Finish triangle.cast                                                                |StilToDo|
 	-Make triangle.cast work for shadows                                                 |StilToDo|
 	-Find the required vectors for the camera when it is created                         |StilToDo|
-	-Finish camera.rotate and camera.getRay to make them actually work with rotation     |StilToDo|
 	-Properly clean up World objects on exit                                             |StilToDo|
 	-Make shadows more realistic (add indirect lighting, integrate into normal cast)     |StilToDo|
 	-Fix issue of no shadows on line of x=0                                              |StilToDo|
+	-Optimize getting rays for camera (maybe don't use sine/cosine)                      |_-_-_-_-|
 	-                                                                                    |_-_-_-_-|
 	-                                                                                    |_-_-_-_-|
 	-Add shadows for plane.cast                                                          |PartDone|
 	-Add reflections to all objects                                                      |PartDone|
 	-Redefine operators and such to work with points (and rays?)  (Object overloading)   |PartDone|
 	-Avoid drawing spheres that are behind the camera                                    |PartDone|
+	-Finish camera.rotate and camera.getRay to make them actually work with rotation     |  Done  |
 	-Fix color issues caused by non-linear association of RGB values and brightness      |  Done  |
 	-Make use of Color objects universal                                                 |  Done  |
 	-Fix shadows!                                                                        |  Done  |
@@ -38,27 +39,27 @@ TODO:                                                                           
 
 void set(int, int, Color);
 
-int displayWidth = 512;//600
-int displayHeight = 512;//420
-Uint32* pixels = new Uint32[displayWidth*displayHeight];
+int windowWidth = 512;//600
+int windowHeight = 512;//420
+Uint32* pixels = new Uint32[windowWidth*windowHeight];
 int frameCount = 0;
 int mouseX = 0;
-int mouseY = -displayHeight/2;
+int mouseY = -windowHeight/2;
 bool print;
 int detail = 1;
 int detailSq = detail*detail;
 
 World world;
-Camera camera(Point(0, 0, -displayWidth*3), displayWidth, 10, 80);
-CRay cRay(Ray(Point(0, 0, 0), Point(0, 0, displayWidth)));
-Object* testTri = new Tri(Point(0, 0, displayWidth*2), Point(displayWidth, 0, displayWidth*2), Point(displayWidth/2, displayWidth/2, displayWidth*5/2), Color(25600, 25600, 25600, 65535));
+Camera camera(Point(0, 0, -windowWidth*3), windowWidth, 0, 0);
+CRay cRay(Ray(Point(0, 0, 0), Point(0, 0, windowWidth)));
+Object* testTri = new Tri(Point(0, 0, windowWidth*2), Point(windowWidth, 0, windowWidth*2), Point(windowWidth/2, windowWidth/2, windowWidth*5/2), Color(25600, 25600, 25600, 65535));
 
 
 void setup(){
-	world.lightList.emplace_back(new Light(Point(0, displayWidth, 0), Color(65535, 65535, 65535, 65535)));
-	world.objList.emplace_back(new Ball(world.lightList[0]->pos, displayHeight/10, world.lightList[0]->color, 0));//lightBall
-	world.objList.emplace_back(new Plane(1, -displayWidth*11/20, displayWidth/3, Color(0, 38400, 38400, 65535), Color(0, 10000, 20000, 65535), 0));//testPlane
-	world.objList.emplace_back(new Ball(Point(0, 0, displayWidth), displayHeight/2, Color(65535, 65535, 65535, 65535), 0));//testBall
+	world.lightList.emplace_back(new Light(Point(0, windowWidth, 0), Color(65535, 65535, 65535, 65535)));
+	world.objList.emplace_back(new Ball(world.lightList[0]->pos, windowHeight/10, world.lightList[0]->color, 0));//lightBall
+	world.objList.emplace_back(new Plane(1, -windowWidth*11/20, windowWidth/3, Color(0, 38400, 38400, 65535), Color(0, 10000, 20000, 65535), 0));//testPlane
+	world.objList.emplace_back(new Ball(Point(0, 0, windowWidth), windowHeight/2, Color(65535, 65535, 65535, 65535), 0));//testBall
 }
 
 
@@ -76,18 +77,21 @@ void renderPixel(int x, int y){
 			bTotal += cRay.color.b;
 		}
 	}
-	set(x+displayWidth/2, displayHeight-1-(y+displayHeight/2)/*optomizing here could cause an off-by-one error with some display sizes*/, Color((int)(rTotal/detailSq), (int)(gTotal/detailSq), (int)(bTotal/detailSq)));
+	set(x+windowWidth/2, windowHeight-1-(y+windowHeight/2)/*optomizing here could cause an off-by-one error with some display sizes*/, Color((int)(rTotal/detailSq), (int)(gTotal/detailSq), (int)(bTotal/detailSq)));
 }
 
 void draw(){
-	camera.pos.z = -(mouseX+displayWidth/2)*4;
-	static_cast<Plane*>(world.objList[1])->dist = mouseY*4;
-	//camera.pos.z = (displayWidth*frameCount/5);
-	//static_cast<Plane*>(world.objList[2])->dist = (-21+frameCount)*displayHeight/20;
-	for(int y = displayHeight-1; y>=0; y --){
-		for(int x = 0; x<displayWidth; x ++){
-			renderPixel(x-displayWidth/2, y-displayHeight/2);
-			//set(x, y, Color(65535*x/displayWidth/2, 0, 0));
+	camera.move(Point(0, 0, mouseY*2));
+	camera.rotate(-M_PI/2*mouseX/windowWidth, 0);
+
+	//camera.pos.z = -(mouseX+windowWidth/2)*4;
+	//static_cast<Plane*>(world.objList[1])->dist = mouseY*4;
+	//camera.pos.z = (windowWidth*frameCount/5);
+	//static_cast<Plane*>(world.objList[2])->dist = (-21+frameCount)*windowHeight/20;
+	for(int y = windowHeight-1; y>=0; y --){
+		for(int x = 0; x<windowWidth; x ++){
+			renderPixel(x-windowWidth/2, y-windowHeight/2);
+			//set(x, y, Color(65535*x/windowWidth/2, 0, 0));
 		}
 	}
 }
@@ -104,24 +108,24 @@ int main(/*int argc, char* args[]*/){
 	SDL_GetCurrentDisplayMode(0, &DM);
 	//printf("width: %i, ", DM.w);
 	//printf("height: %i\n", DM.h);
-	//displayWidth = DM.w;
-	//displayHeight = DM.h;
+	//windowWidth = DM.w;
+	//windowHeight = DM.h;
 	if(SDL_Init(SDL_INIT_VIDEO)<0){
 		printf("SDL could not initialize.  SDL_Error: %s\n", SDL_GetError());
 	}
 	else{
-		window = SDL_CreateWindow("3D Raycaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, displayWidth, displayHeight, 0);//used to end with "SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI" instead of "0"
+		window = SDL_CreateWindow("3D Raycaster", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, 0);//used to end with "SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI" instead of "0"
 		if(window==NULL){
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
 
 		}
 		else{
 			renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
-			buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, displayWidth, displayHeight);
+			buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STATIC, windowWidth, windowHeight);
 			setup();
 			while(!quit){
 				draw();
-				SDL_UpdateTexture(buffer, NULL, pixels, displayWidth*sizeof(Uint32));
+				SDL_UpdateTexture(buffer, NULL, pixels, windowWidth*sizeof(Uint32));
 				SDL_RenderClear(renderer);
 				SDL_RenderCopy(renderer, buffer, NULL, NULL);
 				SDL_RenderPresent(renderer);
@@ -130,7 +134,7 @@ int main(/*int argc, char* args[]*/){
 
 				/*if(frameCount<=20){
 					const char* name = ("frame_"+std::to_string(frameCount)+".bmp").c_str();
-					SDL_SaveBMP(SDL_CreateRGBSurfaceFrom(pixels, displayWidth, displayHeight, 32, displayWidth*4, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000), name);
+					SDL_SaveBMP(SDL_CreateRGBSurfaceFrom(pixels, windowWidth, windowHeight, 32, windowWidth*4, 0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000), name);
 					printf("Saved file %s\n", name);
 				}
 				else{quit = true;}*/
@@ -141,9 +145,9 @@ int main(/*int argc, char* args[]*/){
 							quit = true;
 							break;
 						case SDL_MOUSEMOTION:
-							mouseX = event.motion.x-displayWidth/2;
-							mouseY = -event.motion.y+displayHeight/2;
-							//printf("mouseX=%f, mouseY=%f\n", mouseX*1.0/displayWidth, mouseY*1.0/displayHeight);
+							mouseX = event.motion.x-windowWidth/2;
+							mouseY = -event.motion.y+windowHeight/2;
+							//printf("mouseX=%f, mouseY=%f\n", mouseX*1.0/windowWidth, mouseY*1.0/windowHeight);
 					}
 				}
 			}
@@ -160,7 +164,7 @@ int main(/*int argc, char* args[]*/){
 }
 
 void set(int x, int y, Color color){
-	if(x>=0 && x<displayWidth && y>=0 && y<displayHeight){pixels[y*displayWidth+x] = (((int)sqrt(color.r))<<16)+(((int)sqrt(color.g))<<8)+((int)sqrt(color.b));}
+	if(x>=0 && x<windowWidth && y>=0 && y<windowHeight){pixels[y*windowWidth+x] = (((int)sqrt(color.r))<<16)+(((int)sqrt(color.g))<<8)+((int)sqrt(color.b));}
 	else{printf("Tried to draw pixel out of bounds at (%i, %i)\n", x, y);}
 }
 
--- camera.cpp
+++ camera.cpp
@@ -1,4 +1,4 @@
-//#include <cmath>
+#include <cmath>
 #include <limits>//This library is used to get the float max value.
 #include "rays.h"
 #include "camera.h"
@@ -7,23 +7,27 @@
 
 
 Camera::Camera(Point setPos, float setPlaneDist, float setAngleX, float setAngleY){
-	planeDist = setPlaneDist;
+	planeDist = setPlaneDist;//planeDist effectively sets the field of view of the camera.
 	move(setPos);
 	rotate(setAngleX, setAngleY);
 }
 void Camera::move(Point setPos){
 	pos = setPos;
 }
-void Camera::rotate(float setAngleX, float setAngleY){//This should set the front, up, and right vectors.
-	front.x = 0;//front is a unit vector pointing in the direction that the camera is facing.
-	front.y = 0;
-	front.z = planeDist;
-	up.x = 0;//up is a unit vector pointing directly up from the camera.
-	up.y = 1;
-	up.z = 0;
-	right.x = 1;//right is a unit vector pointing directly to the right the camera.
+void Camera::rotate(float setAngleX, float setAngleY){//Can probably be optomized.  Also, should maybe be passed by reference?
+	front.x = sin(setAngleX)*cos(setAngleY);//front is a vector pointing in the direction that the camera is facing that extends to the plane the camera traces through.
+	front.y = sin(setAngleY);
+	front.z = cos(setAngleX)*cos(setAngleY);
+	front *= planeDist;
+	up.x = sin(setAngleX)*cos(setAngleY+M_PI/2);//up is a unit vector pointing directly up from the camera.
+	up.y = sin(setAngleY+M_PI/2);
+	up.z = cos(setAngleX)*cos(setAngleY+M_PI/2);
+	//up.x = 0;
+	//up.y = 1;
+	//up.z = 0;
+	right.x = sin(setAngleX+M_PI/2);//right is a unit vector pointing directly to the right the camera.
 	right.y = 0;
-	right.z = 0;//These should all be found instead of being set to defaults.
+	right.z = cos(setAngleX+M_PI/2);
 }
 void Camera::getRay(CRay& ray, float screenX, float screenY){
 	ray.ray.p1 = pos;
