77ec28bbda25d9d0a5282244c8d29117a1298731
mroam <1194289+mroam@users.noreply.github.com>
2018-10-19 13:42:08 -0400

    Update rays.cpp
    slight pretty print

--- rays.cpp
+++ rays.cpp
@@ -19,30 +19,39 @@ Point::Point(float setX, float setY, float setZ){
 	z = setZ;
 }
 
+
 Point& Point::operator+=(const Point& toAdd){
 	x += toAdd.x;
 	y += toAdd.y;
 	z += toAdd.z;
 	return *this;
 }
+
+
 Point& Point::operator-=(const Point& toSub){
 	x -= toSub.x;
 	y -= toSub.y;
 	z -= toSub.z;
 	return *this;
 }
+
+
 /*Point& Point::operator*=(const Point& toMult){
 	x *= toMult.x;
 	y *= toMult.y;
 	z *= toMult.z;
 	return *this;
 }*/
+
+
 Point& Point::operator*=(const float toMult){
 	x *= toMult;
 	y *= toMult;
 	z *= toMult;
 	return *this;
 }
+
+
 /*Point& Point::operator/=(const Point& toDiv){
 	x /= toDiv.x;
 	y /= toDiv.y;
@@ -56,6 +65,7 @@ Ray::Ray(Point setP1, Point setP2){
 	p2 = setP2;
 	//length = dist3D(x1, y1, z1, x2, y2, z2);
 }
+
 
 CRay::CRay(Ray setRay){
 	ray = setRay;
@@ -71,8 +81,13 @@ CRay::CRay(Ray setRay){
 	escape = true;
 	bounceCount = 0;
 }
+
+
 void CRay::setColor(Color toSet, Point hit, float dist, bool ignoreDirection){
-	if((ignoreDirection || (ray.p2.x>ray.p1.x==hit.x>ray.p1.x && ray.p2.y>ray.p1.y==hit.y>ray.p1.y && ray.p2.z>ray.p1.z==hit.z>ray.p1.z)) && dist<=setDist){
+	if((ignoreDirection || ((ray.p2.x>ray.p1.x==hit.x>ray.p1.x)
+				&& (ray.p2.y>ray.p1.y==hit.y>ray.p1.y)
+				&& (ray.p2.z>ray.p1.z==hit.z>ray.p1.z))) 
+	   			&& dist<=setDist){
 		setDist = dist;
 		setPos = hit;
 		if(toSet.a>0){
@@ -83,13 +98,18 @@ void CRay::setColor(Color toSet, Point hit, float dist, bool ignoreDirection){
 		setCastColor.a = toSet.a;
 	}
 }
+
+
 void CRay::finishCast(bool doSetPos){
 	if(doSetPos){
 		ray.p2 = setPos;
 	}
-	color.r = (uint64_t)setCastColor.r*setCastColor.a*color.a/4294836225+(uint64_t)color.r*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
-	color.g = (uint64_t)setCastColor.g*setCastColor.a*color.a/4294836225+(uint64_t)color.g*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
-	color.b = (uint64_t)setCastColor.b*setCastColor.a*color.a/4294836225+(uint64_t)color.b*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
+	color.r = (uint64_t)setCastColor.r*setCastColor.a*color.a/4294836225 
+		+ (uint64_t)color.r*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
+	color.g = (uint64_t)setCastColor.g*setCastColor.a*color.a/4294836225
+		+ (uint64_t)color.g*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
+	color.b = (uint64_t)setCastColor.b*setCastColor.a*color.a/4294836225
+		+ (uint64_t)color.b*(4294836225-(uint32_t)setCastColor.a*color.a)/4294836225;
 	color.a = (uint32_t)color.a*(65535-setCastColor.a)/65535;
 	if(color.a==0){escape = false;}
 }
