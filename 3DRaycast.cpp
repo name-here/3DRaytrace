67d4de84b165e33166623708fb8bdef8ded652ea
mroam <1194289+mroam@users.noreply.github.com>
2018-10-19 14:48:41 -0400

    Update 3DRaycast.cpp
    more prettify

--- 3DRaycast.cpp
+++ 3DRaycast.cpp
@@ -1,32 +1,5 @@
-/*
-TODO:                                                                                    |𝔻𝕠𝕟𝕖𝕟𝕖𝕤𝕤|
-	-make README									     |StilToDo|
-	-Add and implement/use ray.length                                                    |Maybe Do|
-	-Separate alpha from color object (not needed for some uses)                         |Maybe Do|
-	-classes.cpp line 182: add position to ball shadow casting(??)                       |StilToDo|
-	-Find the normal of a triangle when it is created                                    |StilToDo|
-	-Finish triangle.cast                                                                |StilToDo|
-	-Make triangle.cast work for shadows                                                 |StilToDo|
-	-Find the required vectors for the camera when it is created                         |StilToDo|
-	-Properly clean up World objects on exit                                             |StilToDo|
-	-Make shadows more realistic (add indirect lighting, integrate into normal cast)     |StilToDo|
-	-Fix issue of no shadows on line of x=0                                              |StilToDo|
-	-Optimize getting rays for camera (maybe don't use sine/cosine)                      |_-_-_-_-|
-	-                                                                                    |_-_-_-_-|
-	-                                                                                    |_-_-_-_-|
-	-Add shadows for plane.cast                                                          |PartDone|
-	-Add reflections to all objects                                                      |PartDone|
-	-Redefine operators and such to work with points (and rays?)  (Object overloading)   |PartDone|
-	-Avoid drawing spheres that are behind the camera                                    |PartDone|
-	-Finish camera.rotate and camera.getRay to make them actually work with rotation     |  Done  |
-	-Fix color issues caused by non-linear association of RGB values and brightness      |  Done  |
-	-Make use of Color objects universal                                                 |  Done  |
-	-Fix shadows!                                                                        |  Done  |
-	-Make use of Point object universal                                                  |  Done  |
-	-Restructure so that world object contains all objects to be cast                    |  Done  |
-	-Figure out mouseX and mouseY polarity                                               |  Done  |
-	-Make the plane cast actually get what axis it is on from the variable               |  Done  |
-*/
+
+// TODO has been moved to stand-alone TODO file
 
 
 #include <SDL2/SDL.h>
@@ -85,8 +58,8 @@ void renderPixel(int x, int y){
 			bTotal += cRay.color.b;
 		}
 	}
-	set(x+windowWidth/2, windowHeight-1-(y+windowHeight/2)
-	    /*optimizing here could cause an off-by-one error with some display sizes*/, 
+	set( x + windowWidth/2, windowHeight - 1 - (y+ (windowHeight/2))
+	    /* Optimizing here could cause an off-by-one error with some display sizes */, 
 	    Color((int)(rTotal/detailSq), (int)(gTotal/detailSq), (int)(bTotal/detailSq)));
 } // renderPixel( )
 
@@ -95,7 +68,7 @@ void renderPixel(int x, int y){
 /* color the specified pixel in pixels[ ] array, if in bounds */
 void set(int x, int y, Color color){
 	if (x>=0 && x<windowWidth && y>=0 && y<windowHeight){
-		pixels[y*windowWidth+x] = (((int)sqrt(color.r))<<16)+(((int)sqrt(color.g))<<8)+((int)sqrt(color.b));}
+		pixels[y*windowWidth+x] = (((int)sqrt(color.r))<<16) + (((int)sqrt(color.g))<<8) + ((int)sqrt(color.b));}
 	else {
 		printf("Tried to draw pixel out of bounds at (%i, %i)\n", x, y);
 	}
