0c64620751e2065ad273d92da31876b7abddded3
name-here <felixjyancey@mac.com>
2019-09-29 20:33:49 -0400

    Changed INTERSECT_ERR back to UNIT/10000

--- world.h
+++ world.h
@@ -2,7 +2,7 @@
 #include <vector>
 
 const double UNIT = 100;//This is determines the size of the units used for size and positioning of all objects.
-const double INTERSECT_ERR = UNIT/1000;//Intersection margin of error (for shapes that need it).  Rays must start this distance away from an object to hit it, to avoid rays bouncing off the same point twice.
+const double INTERSECT_ERR = UNIT/10000;//Intersection margin of error (for shapes that need it).  Rays must start this distance away from an object to hit it, to avoid rays bouncing off the same point twice.
 const int MAX_BOUNCES = 15;//This is the maximum number of bounces the program will calculate.  Further potential reflections are ignored.
 
 class Light;
